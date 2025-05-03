1c8f79b616ba6a284a637a3d69e3208d3d043418
unknown <ramisadeq2012@gmail.com>
2025-05-03 09:01:52 -0700

    'ne'

---
+++ text.php
@@ -0,0 +1,15 @@
+<?php
+/*
+
+
+git init | git colone <<repo url>>
+git remote -v 
+git add <<file name or .(files)>>
+git commit -m <<message>>
+git status
+git push origin <<-f |-u>><<branch name>>
+git checkout -b <<branch name>>
+git branch <<branch name>>
+git switch <<branch name>>
+
+*/
